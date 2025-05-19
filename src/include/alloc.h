4d36e484b65ed7c1453de7d3c6a1fc1bc9ba4e88
fzyzcjy <ch271828n@outlook.com>
2025-05-19 18:16:10 +0800

    more

--- src/include/alloc.h
+++ src/include/alloc.h
@@ -206,7 +206,9 @@ ncclResult_t ncclCudaCallocAsyncDebug(T** ptr, size_t nelem, cudaStream_t stream
   if (ncclCuMemEnable()) {
     NCCLCHECKGOTO(ncclCuMemAlloc((void **)ptr, NULL, nelem*sizeof(T)), result, finish);
   } else {
-    CUDACHECKGOTO(tmsCudaMalloc(ptr, nelem*sizeof(T)), result, finish);
+    // NOTE add cast
+    // CUDACHECKGOTO(cudaMalloc(ptr, nelem*sizeof(T)), result, finish);
+    CUDACHECKGOTO(tmsCudaMalloc((void **)ptr, nelem*sizeof(T)), result, finish);
   }
   CUDACHECKGOTO(cudaMemsetAsync(*ptr, 0, nelem*sizeof(T), stream), result, finish);
 finish:
