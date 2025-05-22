9ac9b7f6b3c99eb8436f967081cd0e04af8e2613
fzyzcjy <ch271828n@outlook.com>
2025-05-22 10:45:04 +0800

    more

--- src/nccl_tms.cc
+++ src/nccl_tms.cc
@@ -1,5 +1,23 @@
 #include "tms.h"
 
+struct NcclTmsRecord {
+
+};
+
+class NcclTmsImpl {
+public:
+    NcclTmsImpl() {}
+
+    static NcclTmsImpl &instance() {
+        static NcclTmsImpl instance;
+        return instance;
+    }
+
+private:
+    std::mutex allocator_metadata_mutex_;
+    std::vector<NcclTmsRecord> records_;
+};
+
 void ncclTmsRegister(void* ptr, size_t size, uint64_t rawIpcDesc, NcclTmsIpcMode ipcMode) {
     TODO;
 }
